4feca243e57c5d61bf7bfb2043ad2914c26f93b4
nshutilancelot3 <nshutilancelot111@gmail.com>
2026-02-06 10:23:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//console.log("Hello World");
+
+//window.alert("this is an alert!");
+//window.alert("i like pizza");
+
+
+// document.getElementById("myp").textContent  = "i like pizza!";
+
+//variables
+//let price = 10.99;
+// let age  = 20;
+// console.log(`your age is ${age} years old`);
+//console.log(`you have to pay${price} today`);
+
+
+//console.log(typeof age);
+//console.log(typeof price);
+// let food = "pizza"
+// console.log(typeof food)
+// let firstname = "john";
+// console.log(typeof firstname);
+
+// console.log(`your name is ${firstname}, your age is ${age} and you like ${food}`);
+
+// let online = false;
+// console.log(`nshuti is online:${online}`)
+
+document.getElementById("myh1").textContent = "Hello";
+
+let name = "nshuti lancelot";
+let age = 20;
+let student = true;
+
+document.getElementById("p1").textContent = `your name is ${name}`;
+document.getElementById("p2").textContent = `and you are ${age} years old`;
+document.getElementById("p3").textContent = `are you a student? ${student}`;
+
+//OPERATORS
+
+let students = 30;
+
+// students = students + 1;
+students = students / 2;
+console.log(students)
+
+
